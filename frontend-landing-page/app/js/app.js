9bfd7a6399484da1afe4c86fc2bed0d8ba62765a
Sunnat Makhmudov <sunnat_m98@icloud.com>
2022-06-25 20:07:36 +0500

    [feat] anchor menu

--- frontend-landing-page/app/js/app.js
+++ frontend-landing-page/app/js/app.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', () => {
 		}
 	}
 
-	for (let i = 0; i <= dataReload.length ; i++) {
+	for (let i = 0; i <= dataReload.length; i++) {
 		console.log("inside")
 		console.log(dataReload[i])
 		dataReload.onclick = function () {
@@ -37,12 +37,34 @@ document.addEventListener('DOMContentLoaded', () => {
 	function clickHandler(e) {
 		e.preventDefault();
 		const href = this.getAttribute("href");
-		const offsetTop = document.querySelector(href).offsetTop;
+		const offsetTop = document.querySelector(href).offsetTop -120;
 
 		scroll({
 			top: offsetTop,
 			behavior: "smooth"
 		});
 	}
+
+	// $("a").on('click', function (event) {
+
+	// 	// Make sure this.hash has a value before overriding default behavior
+	// 	if (this.hash !== "") {
+	// 		// Prevent default anchor click behavior
+	// 		event.preventDefault();
+
+	// 		// Store hash
+	// 		var hash = this.hash;
+
+	// 		// Using jQuery's animate() method to add smooth page scroll
+	// 		// The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+	// 		$('html, body').animate({
+	// 			scrollTop: $(hash).offset().top
+	// 		}, 800, function () {
+
+	// 			// Add hash (#) to URL when done scrolling (default click behavior)
+	// 			window.location.hash = hash;
+	// 		});
+	// 	} // End if
+	// });
 
 })
